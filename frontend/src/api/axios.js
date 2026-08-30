c3ed02e88ccac81bd02177338bf5fefad82ff2f9
Reshal Maryson <reshalmaryson000@gmail.com>
2026-08-30 14:51:18 +0500

    UI enhancment - 3.3

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,8 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  // baseURL: "https://criticai.onrender.com",
-    baseURL: "http://localhost:5000",
+  baseURL: "https://criticai.onrender.com",
   withCredentials: true,
 });
 
